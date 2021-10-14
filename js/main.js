df4e5cfe227af6070ad7ac9ec0206acf937e9b5a
Marina Ivantsova <marineivantsova@yandex.ru>
2021-10-14 23:24:32 +0300

    linter without console

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ const getRandomInteger = function(min = 0, max = 0) {
   return Math.random() * (max - min) + min;
 };
 
-console.log(Math.floor(getRandomInteger(4, 8)));
+Math.floor(getRandomInteger(4, 8));
 
 const checkMaxLength = function(checkedString, maxLength) {
   return (checkedString.length <= maxLength);
 };
 
-console.log(checkMaxLength('Ехал грека через реку', 25));
+checkMaxLength('Ехал грека через реку', 25);
