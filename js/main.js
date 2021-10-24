d1a4221b5653b5a6b293c4cbb5005ea51bdafe2f
Marina Ivantsova <marineivantsova@yandex.ru>
2021-10-24 19:34:05 +0300

    linter

--- js/main.js
+++ js/main.js
@@ -11,12 +11,12 @@ const MESSAGE_MOCK = [
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 const NAMES_MOCK = [
   'Иван',
   'Петя',
-  'Ольга'
+  'Ольга',
 ];
 
 const getRandomInteger = function (min = 0, max = 0) {
@@ -31,24 +31,24 @@ checkMaxLength('Ехал грека через реку', 25);
 
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
-};
+}
 
 const arr = [];
 let int = 0;
-for (i = 0; i < 1000; i++) {
+for (let i = 0; i < 1000; i++) {
   arr.push(i);
 }
 shuffle(arr);
 
 const createComments = function (maxLength) {
   const result = [];
-  for (let i = 0; i < maxLength; i++) {
+  for (let integer = 0; i < maxLength; integer++) {
     result.push(
       {
         id: arr[int++],
         avatar: AVATAR_START + getRandomInteger(1, 6) + AVATAR_EXTEND,
         message: MESSAGE_MOCK[getRandomInteger(0, MESSAGE_MOCK.length - 1)],
-        name: NAMES_MOCK[getRandomInteger(0, NAMES_MOCK.length - 1)]
+        name: NAMES_MOCK[getRandomInteger(0, NAMES_MOCK.length - 1)],
       }
     )
   };
@@ -57,15 +57,14 @@ const createComments = function (maxLength) {
 
 const createData = function (maxLength) {
   const result = [];
-  for (let i = 1; i <= maxLength; i++) {
+  for (let integer = 1; i <= maxLength; integer++) {
     result.push({
-      id: i,
-      url: IMG_START + i + IMG_EXTEND,
+      id: integer,
+      url: IMG_START + integer + IMG_EXTEND,
       description: 'Ехал грека через реку',
       likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
-      comments: createComments(getRandomInteger(2, 5))
-    })
+      comments: createComments(getRandomInteger(2, 5)),
+    });
   }
   return result;
-}
-console.log(createData(MAX_ITEM));
+};
