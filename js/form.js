e6973a383d45d06fc55d0946882df1e0b6ffe743
Marina Ivantsova <marineivantsova@yandex.ru>
2022-03-09 11:55:34 +0300

    Баг валидации исправлен

--- js/form.js
+++ js/form.js
@@ -6,10 +6,8 @@ const hashtagElement = document.querySelector('.text__hashtags');
 const MAX_HASHTAG_NUMBERS = 5;
 
 
-const validateHashtag = function (currentHashTag, nextHashTag) {
+const validateHashtag = function (currentHashTag) {
   const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
-  const currentHashtag = currentHashTag.toLowerCase();
-  const nextHashtag = nextHashTag.toLowerCase();
 
   if (currentHashTag.length === 1 && (currentHashTag === '#')) {
     hashtagElement.setCustomValidity('Хеш-тег не может состоять только из одной решётки.');
@@ -17,24 +15,29 @@ const validateHashtag = function (currentHashTag, nextHashTag) {
   else if (!re.test(currentHashTag)) {
     hashtagElement.setCustomValidity('Хэш-тег начинается с символа #, не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д., максимальная длина одного хэш-тега 20 символов, включая решётку.');
   }
-  else if (currentHashtag === nextHashtag) {
+
+};
+
+const validateSameHashtags = function (current, next) {
+  if (current === next) {
     hashtagElement.setCustomValidity('#ХэшТег и #хэштег считаются одним и тем же тегом, теги не должны повторяться.');
-  }
-  else {
-    hashtagElement.setCustomValidity('');
   }
 };
 
-const onUploadForm = function () {
 
+const onUploadForm = function () {
   const hashtags = hashtagElement.value;
+  hashtagElement.setCustomValidity('');
   const arrayOfHashtags = hashtags.split(' ');
   if (arrayOfHashtags.length > MAX_HASHTAG_NUMBERS) {
     hashtagElement.setCustomValidity(`Хэш-тегов не должно быть ${MAX_HASHTAG_NUMBERS}`);
   } else {
     for (let i = 0; i < arrayOfHashtags.length; i++) {
+      validateHashtag(arrayOfHashtags[i]);
       for (let j = i + 1; j < arrayOfHashtags.length; j++) {
-        validateHashtag(arrayOfHashtags[i], arrayOfHashtags[j]);
+        const currentHashtag = arrayOfHashtags[i].toLowerCase();
+        const nextHashtag = arrayOfHashtags[j].toLowerCase();
+        validateSameHashtags(currentHashtag, nextHashtag);
       }
     }
   }
